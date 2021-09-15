8139bb7fdfca1df4b622b5f83630e9a2e8caa128
Reskyppo <reskybeen@gmail.com>
2021-09-15 19:47:25 +0700

    [add] new color primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: "#6F69AC",
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      blue: colors.blue,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
   },
   variants: {
     extend: {},
