d5ea9d49c7b1e94a5980d83874ce02a3ef600c85
Reskyppo <reskybeen@gmail.com>
2021-09-16 09:58:05 +0700

    [done] homepage ui prototypes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,24 +5,26 @@ const Home = () => {
     <div className="bg-gray-100 h-screen flex justify-center ">
       <div className=" w-1/2  border-primary border-r-2 border-l-2 bg-white p-8">
         <img
-          className="w-3/4 mx-auto"
+          className="w-11/12 my-20 mx-auto"
           src={HeroIcon}
           alt="laptop illustration that represent about online quiz"
         />
 
-        <div className="w-11/12 mx-auto mt-12">
-          <p className="text-center text-4xl text-primary">True Or False</p>
-          <p className="text-center text-blue-800">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A odio
-            consequuntur consectetur autem facere? Porro, ipsa aliquid debitis
-            maiores doloremque ullam eum inventore incidunt voluptatibus
-            asperiores expedita repudiandae temporibus animi.
+        <div className="w-11/12 mx-auto px-6 mb-12">
+          <p className=" text-4xl text-primary-900 font-semibold">
+            Feeling bored and want to test your intelligence?
+          </p>
+          <p className=" text-primary-500 font-medium text-xl mt-4">
+            By answering these question, we can help you to test your
+            intelligence and increase your knowledge.
           </p>
         </div>
 
-        <div className="flex justify-center">
-          <button className="bg-secondary w-2/3 rounded-md cursor-pointer">
-            <p className="py-4 text-center text-2xl text-white">Start quiz</p>
+        <div className="flex justify-center mt-16">
+          <button className="bg-secondary-900 w-5/6 rounded-md cursor-pointer">
+            <p className="py-4 text-center text-2xl font-medium text-white">
+              Start quiz
+            </p>
           </button>
         </div>
       </div>
