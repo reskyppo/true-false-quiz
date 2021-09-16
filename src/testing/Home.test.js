1acf80e500c3523fe4e86304d24887ec43134d19
Reskyppo <reskybeen@gmail.com>
2021-09-16 10:00:38 +0700

    add app desc and changes test case

--- src/testing/Home.test.js
+++ src/testing/Home.test.js
@@ -4,10 +4,19 @@ import Home from "../pages/Home";
 describe("Homepage", () => {
   test("Render app title", () => {
     render(<Home />);
-    const linkElement = screen.getByText("True Or False");
+    const linkElement = screen.getByText(
+      /Feeling bored and want to test your intelligence?/i
+    );
     expect(linkElement).toBeInTheDocument();
   });
-  test('Icon must have src = "../assets/hero_icon.svg" and alt = "laptop illustration that represent about online quiz"', () => {
+  test("Render app description", () => {
+    render(<Home />);
+    const linkElement = screen.getByText(
+      /By answering these question, we can help you to test your intelligence and increase your knowledge./i
+    );
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('Icon must have src = "hero_icon.svg" and alt = "laptop illustration that represent about online quiz"', () => {
     render(<Home />);
     const icon = screen.getByRole("img");
     expect(icon).toHaveAttribute("src", "hero_icon.svg");
