199c279acd0be2f33c7d7711a25e92e33c2e601b
Reskyppo <reskybeen@gmail.com>
2021-09-16 13:14:17 +0700

    [done] 404 not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,25 @@
+import NotfoundIcon from "../assets/not_found.svg";
+
 const NotFound = () => {
   return (
-    <div>
-      <p className="text-red-900">this is 404 page</p>
+    <div className="bg-gray-100 h-screen flex justify-center items-center px-4">
+      <div>
+        <img
+          src={NotfoundIcon}
+          alt="Icon that tell you, this website not found and you can't access it."
+        />
+        <p className="text-center my-4 text-lg md:text-2xl">
+          The page you are looking for does not exist!
+        </p>
+
+        <div className="flex justify-center mt-8">
+          <button className="bg-primary-500 rounded-md cursor-pointer">
+            <p className="py-2 md:py-4 px-4 md:px-8 text-center md:text-xl font-medium text-white">
+              GO BACK HOME
+            </p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
