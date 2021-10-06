6d4ef4555bd45140452c4620cd015ce91f8a1061
Reskyppo <reskybeen@gmail.com>
2021-10-06 22:26:31 +0700

    add detail component and mapping json

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,29 +1,18 @@
-import { useState } from "react";
-
+import datas from "../utils/data.json";
+import QuizDetails from "../components/QuizDetails";
 const Quiz = () => {
-  const [answer, setAnswer] = useState();
-  console.log(answer);
+  console.log(datas);
   return (
     <div className="bg-gray-100 h-screen flex justify-center ">
       <div className=" w-1/2  border-primary border-r-2 border-l-2 bg-white p-8">
-        <p className="text-xl">Question 1 / 5</p>
-        <div className="flex">
-          <span className="h-4 w-1/5 bg-secondary-700 "></span>
-          <span className="h-4 w-4/5 bg-primary-100"></span>
-        </div>
-
-        <div className="mt-8">
-          <p>Questions</p>
-
-          <div className="mt-8 flex flex-col">
-            <button onClick={() => setAnswer(true)}>True</button>
-            <button onClick={() => setAnswer(false)}>False</button>
-          </div>
-
-          <div className="mt-8">
-            <button className="bg-primary-500">Next</button>
-          </div>
-        </div>
+        {datas?.map((data) => (
+          <QuizDetails
+            number={data.number}
+            total={datas.length}
+            question={data.question}
+            answer={data.answer}
+          />
+        ))}
       </div>
     </div>
   );
