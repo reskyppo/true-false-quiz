6320ecf32eee4094fe3ae93845261801ccf0a3e0
Reskyppo <reskybeen@gmail.com>
2021-10-08 16:30:47 +0700

    add progressbar helper comp

---
+++ src/components/Progressbar.jsx
@@ -0,0 +1,19 @@
+const Progressbar = ({ currentPage, totalPage }) => {
+  return (
+    <div className="flex">
+      <span
+        className={`h-4 w-${currentPage}/${totalPage} bg-secondary-700 `}
+      ></span>
+      <span
+        className={`h-4 w-${
+          totalPage - currentPage
+        }/${totalPage} bg-primary-100`}
+      ></span>
+      {totalPage === currentPage && (
+        <span className={`h-4 w-full bg-secondary-700`}></span>
+      )}
+    </div>
+  );
+};
+
+export default Progressbar;
