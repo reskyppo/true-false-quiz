d47ebe2706bf94c5690cf239f782e67397d4671d
Reskyppo <reskybeen@gmail.com>
2021-10-08 16:31:02 +0700

    refactoring quiz page

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,18 +1,43 @@
 import datas from "../utils/data.json";
-import QuizDetails from "../components/QuizDetails";
+import { useState } from "react";
+import Progressbar from "../components/Progressbar";
 const Quiz = () => {
-  console.log(datas);
+  const [userAnswer, setUserAnswer] = useState();
+  const [currentPage, setCurrentPage] = useState(0);
+  const totalPage = datas.length;
   return (
     <div className="bg-gray-100 h-screen flex justify-center ">
       <div className=" w-1/2  border-primary border-r-2 border-l-2 bg-white p-8">
-        {datas?.map((data) => (
-          <QuizDetails
-            number={data.number}
-            total={datas.length}
-            question={data.question}
-            answer={data.answer}
-          />
-        ))}
+        <p className="text-xl">
+          Question {datas[currentPage].number} / {totalPage}
+        </p>
+
+        <Progressbar
+          currentPage={datas[currentPage].number}
+          totalPage={totalPage}
+        />
+
+        <div className="mt-8">
+          <p>{datas[currentPage].question}</p>
+          <div className="mt-8 flex flex-col">
+            <button onClick={() => setUserAnswer(true)}>True</button>
+            <button onClick={() => setUserAnswer(false)}>False</button>
+          </div>
+
+          {userAnswer === datas[currentPage].answer && <p>yeay</p>}
+          <div className="mt-8">
+            {currentPage + 1 !== totalPage ? (
+              <button
+                className="bg-primary-500"
+                onClick={() => setCurrentPage(currentPage + 1)}
+              >
+                Next
+              </button>
+            ) : (
+              <button className="bg-primary-500">Submit</button>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
