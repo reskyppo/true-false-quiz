90b06c63dfca31b83a78966de9f4c1d3466bb85b
Reskyppo <reskybeen@gmail.com>
2021-10-09 21:12:04 +0700

    implement redux

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -2,22 +2,23 @@ import datas from "../utils/data.json";
 import { useState } from "react";
 import Progressbar from "../components/Progressbar";
 import { useHistory } from "react-router";
-
+import { useDispatch } from "react-redux";
+import { addScore } from "../features/scores/scoresSlice";
 const Quiz = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const [userAnswer, setUserAnswer] = useState();
   const [currentPage, setCurrentPage] = useState(0);
-  const [score, setScore] = useState(0);
 
   const totalPage = datas.length;
 
   const handleScore = () => {
-    userAnswer === datas[currentPage].answer && setScore(score + 1);
+    userAnswer === datas[currentPage].answer && dispatch(addScore());
     currentPage + 1 === totalPage && history.push("/result");
     setCurrentPage(currentPage + 1);
   };
-  
+
   return (
     <div className="bg-gray-100 h-screen flex justify-center ">
       <div className=" w-1/2  border-primary border-r-2 border-l-2 bg-white p-8">
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,7 +1,10 @@
+import { useSelector } from "react-redux"
+
 const Result = () => {
+const score = useSelector(state => state.scores.score)
   return (
     <div>
-      ini result
+      {score}
     </div>
   )
 }
