ca83ccc1e108deac1c6146db1deef9b788b0faae
Reskyppo <reskybeen@gmail.com>
2021-10-09 09:34:49 +0700

    add result routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Quiz from "./pages/Quiz";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Result from "./pages/Result";
 const App = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/quiz" component={Quiz} />
+          <Route path="/result" component={Result} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
