25d800532a29933f49696ee9b947247c0c9a7bf7
Reskyppo <reskybeen@gmail.com>
2021-10-31 20:08:35 +0700

    wrap Link with Router

--- src/testing/Home.test.js
+++ src/testing/Home.test.js
@@ -1,23 +1,37 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../pages/Home";
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Home from '../pages/Home';
 
 describe("Homepage", () => {
   test("Render app title", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const linkElement = screen.getByText(
       /Feeling bored and want to test your intelligence?/i
     );
     expect(linkElement).toBeInTheDocument();
   });
   test("Render app description", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const linkElement = screen.getByText(
       /By answering these question, we can help you to test your intelligence and increase your knowledge./i
     );
     expect(linkElement).toBeInTheDocument();
   });
   test('Icon must have src = "hero_icon.svg" and alt = "laptop illustration that represent about online quiz"', () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const icon = screen.getByRole("img");
     expect(icon).toHaveAttribute("src", "hero_icon.svg");
     expect(icon).toHaveAttribute(
@@ -27,7 +41,11 @@ describe("Homepage", () => {
   });
 
   test("Button clickable", () => {
-    render(<Home />);
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
     const button = screen.getByRole("button");
     expect(button).not.toBeDisabled();
   });
