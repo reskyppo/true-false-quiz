0ca08c9725be069f03ebcdc49e0f7643ca6c911f
Reskyppo <reskybeen@gmail.com>
2021-10-31 10:27:49 +0700

    change color progressbar

--- src/components/Progressbar.jsx
+++ src/components/Progressbar.jsx
@@ -2,7 +2,7 @@ const Progressbar = ({ currentPage, totalPage }) => {
   return (
     <div className="flex">
       <span
-        className={`h-4 w-${currentPage}/${totalPage} bg-secondary-500 `}
+        className={`h-4 w-${currentPage}/${totalPage} bg-secondary-300 `}
       ></span>
       <span
         className={`h-4 w-${
@@ -10,7 +10,7 @@ const Progressbar = ({ currentPage, totalPage }) => {
         }/${totalPage} bg-primary-100`}
       ></span>
       {totalPage === currentPage && (
-        <span className={`h-4 w-full bg-secondary-500`}></span>
+        <span className={`h-4 w-full bg-secondary-300`}></span>
       )}
     </div>
   );
