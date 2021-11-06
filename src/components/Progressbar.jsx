f27f5ac44f7fbcc18dd42c5a7a6649f58523c7bd
Reskyppo <reskybeen@gmail.com>
2021-11-07 06:35:31 +0700

    change width to inline style

--- src/components/Progressbar.jsx
+++ src/components/Progressbar.jsx
@@ -2,12 +2,10 @@ const Progressbar = ({ currentPage, totalPage }) => {
   return (
     <div className="flex">
       <span
-        className={`h-4 w-${currentPage}/${totalPage} bg-secondary-300 `}
+        className="h-4 bg-secondary-300" style={{"width": currentPage/totalPage * 100 + "%"}}
       ></span>
       <span
-        className={`h-4 w-${
-          totalPage - currentPage
-        }/${totalPage} bg-gray-300`}
+        className="h-4 bg-gray-300" style={{"width": (totalPage-currentPage)/totalPage * 100 + "%"}}
       ></span>
       {totalPage === currentPage && (
         <span className={`h-4 w-full bg-secondary-300`}></span>
